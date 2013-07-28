f63a196f9062d310f072856e697da1bfda98f9e2
tokiwoousaka <its.out.of.tune.this.my.music@gmail.com>
2013-07-28 13:04:38 +0900

    LambdaExpr作成開始

--- src/Language/CatCat/Parser/LambdaParser.hs
+++ src/Language/CatCat/Parser/LambdaParser.hs
@@ -1,13 +1,13 @@
 module Language.CatCat.Parser.LambdaParser where
 import Language.CatCat.Parser.TypeParser
+import Language.CatCat.Parser.Common 
 
 -------------------
 -- Syntax tree
 
-data Term t v = LambdaTerm (LambdaExpr t v) | TypeExpr t
-
 newtype LambdaVarName = LambdaVarName { getLambdaVarName :: String }
   deriving (Show, Read, Eq)
-data LambdaExpr t v = LambdaExpr --TODO
- 
-  
+data LambdaExpr t v 
+  = LambdaForAll t (LambdaExpr t v)
+  | LambdaArg v (Either t (TypeExpr t)) (LambdaExpr t v)
+  | LambdaDefName DefinedName (LambdaExpr t v)
